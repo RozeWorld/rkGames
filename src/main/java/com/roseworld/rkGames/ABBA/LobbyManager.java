209ce32e22f350b8849fd81959628778fb4bff74
Alexandar Goranov <alex800kill@gmail.com>
2025-07-26 03:20:38 +0300

    Command improvements and changes

--- src/main/java/com/roseworld/rkGames/ABBA/LobbyManager.java
+++ src/main/java/com/roseworld/rkGames/ABBA/LobbyManager.java
@@ -3,13 +3,29 @@ package com.roseworld.rkGames.ABBA;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 public interface LobbyManager {
     List<Lobby> lobbies = new ArrayList<>();
+    HashMap<String, Lobby> lobbiesInvites = new HashMap<>();
+
+    static void addInvite(Lobby lobby, String invite){
+        lobbiesInvites.put(invite, lobby);
+    }
+
+    static void acceptInvite(String invite, Player player){
+        lobbiesInvites.get(invite).addPlayer(player);
+        lobbiesInvites.remove(invite);
+    }
 
     static List<Lobby> getLobbies() {
         return lobbies;
+    }
+
+    static Collection<String> getInvites() {
+        return lobbiesInvites.keySet();
     }
 
     default Lobby getLobby(String name){
@@ -22,6 +38,13 @@ public interface LobbyManager {
     default boolean isPlayerInLobby(Player player){
         for(Lobby lobby : lobbies){
             if(lobby.hasPlayer(player)) return true;
+        }
+        return false;
+    }
+
+    default boolean isLobbyCreator(Player player){
+        for(Lobby lobby : lobbies){
+            if(lobby.getCreator().equals(player.getUniqueId())) return true;
         }
         return false;
     }
@@ -46,6 +69,10 @@ public interface LobbyManager {
     };
 
     default void removeLobby(Lobby lobby){
+        List<Player> players = lobby.getPlayers();
+        lobby.skipProtocol();
+        lobbiesInvites.values().remove(lobby);
         lobbies.remove(lobby);
+        players.forEach(Player::updateCommands);
     }
 }
